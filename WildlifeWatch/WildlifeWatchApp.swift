27f4e6f492d263754c1e13964993c4a10800ef10
Grace <gracezhang5987@gmail.com>
2024-07-29 14:15:32 -0400

    Initial Commit

---
+++ WildlifeWatch/WildlifeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildlifeWatchApp.swift
+//  WildlifeWatch
+//
+//  Created by Jieping Li on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WildlifeWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
