98e50c68890bbcffe23547f1e9ef339d593d9c7e
avnilende <armadillolegs@gmail.com>
2024-07-30 11:35:40 -0400

    Update ContentView.swift

--- WildlifeWatch/ContentView.swift
+++ WildlifeWatch/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Wildlife Watchers")
-        }
-        .padding()
+            NavigationStack {
+                VStack {
+                    Text("animal")
+                    NavigationLink(destination: animal1()) {
+                            Text("animal1!")
+                    }
+                    .navigationTitle("Home")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarHidden(true)
+
+
+                    }
+                }
     }
 }
 
